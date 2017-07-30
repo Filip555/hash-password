d17de7b740606a3141802d9d43ebc13e7316afc1
Pawel Filipek <pawel.fil555@gmail.com>
2017-07-30 12:10:45 +0200

    fc

---
+++ HashPassword.Tests/HashPwTest.cs
@@ -0,0 +1,32 @@
+using System;
+using Xunit;
+
+namespace HashPassword.Tests
+{
+    public class HashPwTest
+    {
+        [Fact]
+        public void Hash_system_should_return_the_correct_hashed_password()
+        {
+            const string password = "secretpw";
+            var hashSystem = new HashPw();
+
+            var salt = "CV5QNYON27MkyFrbiZViitcxfdXZHdHvsE7kSAu4UQ5f9GkxmhMszA ==";
+            var hashPassword = hashSystem.GetHash(password, salt);
+
+            Assert.Equal("AGd+jMv/HauI86d9nRSa568irLdYgWjrHfVALjQ1g04VJLSUucMnEA==", hashPassword);
+        }
+        [Fact]
+        public void Hash_system_all_time_genereate_the_same_hash_password_if_we_give_him_the_same_salt()
+        {
+            const string password = "secretpw";
+            var hashSystem = new HashPw();
+
+            var salt = hashSystem.GetSalt();
+            var hashPassword = hashSystem.GetHash(password, salt);
+            var secondHashPassword = hashSystem.GetHash(password, salt);
+
+            Assert.Equal(hashPassword, secondHashPassword);
+        }
+    }
+}
---
+++ HashPassword/HashPw.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace HashPassword
+{
+    public class HashPw
+    {
+        private const int DeriveBytesIterationsCount = 10000;
+
+        public string GetSalt()
+        {
+            var saltInBytes = new byte[40];
+            var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(saltInBytes);
+            return Convert.ToBase64String(saltInBytes);
+        }
+
+        private static byte[] GetBytes(string value)
+        {
+            var bytes = new byte[value.Length * sizeof(char)];
+            Buffer.BlockCopy(value.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        public string GetHash(string password, string salt)
+        {
+            var hash = new Rfc2898DeriveBytes(password, GetBytes(salt), 1000);
+            return Convert.ToBase64String(hash.GetBytes(40));
+        }
+    }
+}
